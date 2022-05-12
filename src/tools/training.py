dccdd096886a77ac9bce0b21d09733f9a35c5f98
cmetzner93 <cmetzner@vols.utk.edu>
2022-05-12 10:35:25 -0400

    pytorch implementation to train and validate model

---
+++ src/tools/training.py
@@ -0,0 +1,234 @@
+"""
+This file contains source code for the training procedure of the models.
+    @author: Christoph Metzner
+    @email: cmetzner@vols.utk.edu
+    @created: 05/03/2022
+    @last modified: 05/05/2022
+"""
+
+# built-in libraries
+import time
+from typing import Dict, Union
+
+# installed libraries
+import torch
+import torch.nn.functional as F
+import numpy as np
+
+# Custom libraries
+from .performance_metrics import get_scores
+
+# Select GPU as hardware if available otherwise use available CPU
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(f'The experiment uses the following device: {device}')
+
+
+def train(model,
+          train_kwargs: Dict[str, Union[bool, int]],
+          optimizer,
+          train_loader,
+          val_loader=None,
+          class_weights: np.array = None,
+          save_name: str = None):
+    """
+    This function handles training and validating the model using the given training and validation datasets.
+
+    Parameters
+    ----------
+    model : nn.Model
+        Multi-label or multi-class classification model implemented in pytorch using nn.Model
+    train_kwargs : Dict[str, Union[bool, int]]
+        Dictionary storing all variables required to run training/validating process.
+    optimizer : pytorch optimizer
+        Optimizer used for controlling parameter training via backward propagation
+    train_loader : pytorch data loader
+        Dataloader containing the training dataset; samples X and ground-truth values Y
+    val_loader : pytorch data loader
+        Dataloader contianing the validation dataset; samples X and ground-truth values Y
+    class_weights : np.array
+        Array containing the class weights in shape (n_classes,)
+    save_name : str
+        Descriptive name to save the trained model
+
+    """
+
+    epochs = train_kwargs['epochs']
+    patience = train_kwargs['patience']
+    multilabel = train_kwargs['multilabel']
+
+    # Set up loss function
+    class_weights_tensor = None
+    if class_weights is not None:
+        class_weights_tensor = torch.FloatTensor(class_weights).to(device)
+    if multilabel:
+        # https://pytorch.org/docs/stable/generated/torch.nn.BCEWithLogitsLoss.html
+        # Multilabel requires using the sigmoid function to compute pseudo-probabilities ranging [0, 1] for each label
+        # Use BCEWithLogitsLoss() for increased numerical stability
+        loss_fct = torch.nn.BCEWithLogitsLoss(weight=class_weights_tensor)
+    else:
+        # Multiclass - https://pytorch.org/docs/stable/generated/torch.nn.CrossEntropyLoss.html
+        loss_fct = torch.nn.CrossEntropyLoss(weight=class_weights_tensor)
+
+    # Variables to track validation performance and early stopping
+    best_val_loss = np.inf
+    patience_counter = 0
+
+    ### Train model ###
+    for epoch in range(epochs):
+        print(f'Epoch: {epoch + 1}')
+        # Enable training of layers with trainable parameters
+        model.train(True)
+
+        # Init arrays to keep track of ground-truth labels
+        y_trues = []
+        y_preds = []
+
+        # Keep track of training time
+        start_time = time.time()
+        for b, batch in enumerate(train_loader):
+            print(f'Batch: {b}')
+            # set gradients to zero for every new batch
+            optimizer.zero_grad()
+
+            # Cast samples to device; token2id mapped and 0-padded documents of current batch
+            X = batch['X'].to(device)
+
+            # Cast ground-truth labels to device; multi-label or multi-class tensors
+            # Multi-label: multi-hot vectors; multi-class: class indices (tensor([0,1,2,3,4,5])
+            Y = batch['Y'].to(device)
+
+            logits = model(X)
+            loss = 0
+
+            y_trues.extend(Y.detach().cpu().numpy())
+            y_preds.extend(logits.detach().cpu().numpy())  # how do you have to compute these things for multi-class case
+            loss += loss_fct(logits, Y)
+
+            # Perform backpropagation
+            loss.backward()
+            optimizer.step()
+
+            l_cpu = loss.cpu().detach().numpy()
+            if b == 1:
+                break
+        print(f'Training loss: {l_cpu} ({time.time() - start_time:.2f} sec)')
+
+
+        ### Validate model ###
+        if val_loader is not None:
+            print('\nValidating current model with validation dataset.')
+            scores = scoring(model=model,
+                             data_loader=val_loader,
+                             class_weights=class_weights,
+                             multilabel=multilabel)
+            val_loss = scores['loss']
+
+            ### Early stopping to prevent overfitting ###
+            if val_loss < best_val_loss:
+                best_val_loss = val_loss
+                patience_counter = 0
+                torch.save(model.state_dict(), f'{save_name}.pt')
+            else:
+                patience_counter += 1
+                if patience_counter >= patience:
+                    print('Early stopping! No improvement in validation performance!')
+                    break
+    # If no validation dataset available, save after every epoch
+        else:
+            torch.save(model.state_dict(), f'{save_name}.pt')
+
+
+def scoring(model,
+            data_loader,
+            multilabel: bool,
+            class_weights: np.array = None) -> Dict[str, Union[float, np.array]]:
+
+    """
+    Parameters
+    ----------
+    model : nn.Model
+        Multi-label or multi-class classification model implemented in pytorch using nn.Model
+    data_loader : pytorch data loader
+        Dataloader containing the training dataset; samples X and ground-truth values Y
+    multilabel : bool
+        Flag indicating whether multi-label or multi-class text classification is taken place
+    class_weights : np.array
+        Array containing the class weights in shape (n_classes,)
+
+    Returns
+    -------
+    Dict[str, Union[float, np.array]]
+        Dictionary containing the computed performance metrics (scores) and ground-truth values, prediction probabilites
+        prediction, and validating/testing loss.
+    """
+
+    # Set up loss function
+    class_weights_tensor = None
+    if class_weights is not None:
+        class_weights_tensor = torch.FloatTensor(class_weights).to(device)
+    if multilabel:
+        # Multilabel requires using the sigmoid function to compute pseudo-probabilities ranging [0, 1] for each label
+        # Use BCEWithLogitsLoss() for increased numerical stability
+        loss_fct = torch.nn.BCEWithLogitsLoss(weight=class_weights_tensor)
+    else:
+        # Multiclass
+        loss_fct = torch.nn.CrossEntropyLoss(weight=class_weights_tensor)
+
+    # Put model in evaluation mode; turns off stochastic based layers (e.g., dropout or batch normalization)
+    model.eval()
+
+    # Init arrays to keep track of ground-truth labels and predictions
+    y_trues = []
+    y_probs = []
+    y_preds = []
+
+    # Init list to keep track of losses per batch and running validation loss variable
+    losses = []
+
+    # switch off autograd engine; reduces memory usage and increase computation speed
+    with torch.no_grad():
+        # loop through dataset
+        for b, batch in enumerate(data_loader):
+            # Retrieve token2id mapped and 0-padded documents of current batch
+            X = batch['X'].to(device)
+            Y = batch['Y'].to(device)
+
+            logits = model(X)
+            loss = 0
+
+            # Extend arrays with ground-truth values (Y), prediction probabilities (probs), and predictions (logits)
+            y_trues.extend(Y.detach().cpu().numpy())
+            if multilabel:
+                probs = torch.sigmoid(logits).detach().cpu().numpy()
+                y_probs.extend(probs)
+                y_preds.extend(np.round(probs, 0))
+            else:
+                soft_out = F.softmax(logits, dim=-1).max(-1)
+                probs = soft_out[0].detach().cpu().numpy()
+                probs_idx = soft_out[1].detach().cpu().numpy()
+                y_probs.extend(probs)
+                y_preds.extend(probs_idx)
+
+            # Compute the loss for current batch
+            loss += loss_fct(logits, Y)
+            l_cpu = loss.cpu().detach().numpy()
+            losses.append(l_cpu)
+            if b == 1:
+                break
+
+    # Compute the scores
+    scores = {}
+    scores, y_preds_, y_trues_, y_probs_ = get_scores(y_preds_=y_preds,
+                                                      y_trues_=y_trues,
+                                                      y_probs_=y_probs,
+                                                      scores=scores,
+                                                      ks=[5, 8, 15])
+
+    loss = np.mean(losses)
+
+    scores['y_trues'] = y_trues_
+    scores['y_probs'] = y_probs_
+    scores['y_preds'] = y_preds_
+    scores['loss'] = loss
+
+    return scores
