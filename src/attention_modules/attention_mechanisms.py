c33fb9a9f5c7fbaa05eb58939526bb6ef03cc473
cmetzner93 <cmetzner@vols.utk.edu>
2022-05-12 12:50:40 -0400

    added alternating attention mechanism

--- src/attention_modules/attention_mechanisms.py
+++ src/attention_modules/attention_mechanisms.py
@@ -90,7 +90,7 @@ class TargetAttention(nn.Module):
         # Compute attention weighted document embeddings - context matrix
         # Where c_i represents the document context vector for the i-th label in the label space
         # C ∈ R^nxd, where n: number of labels and d: latent dimension of CNN/LSTM model
-        C = A.matmul(V)
+        C = A.matmul(V.permute(0, 2, 1))
         return C, A
 
 
@@ -178,7 +178,7 @@ class SelfAttention(nn.Module):
         # Compute attention weighted document embeddings - context matrix
         # Where c_i represents the document context vector for the i-th label in the label space
         # C ∈ R^nxd, where n: number of labels and d: latent dimension of CNN/LSTM model
-        C = A.matmul(V)
+        C = A.matmul(V.permute(0, 2, 1))
         return C, A
 
 
@@ -264,6 +264,108 @@ class LabelAttention(nn.Module):
         # Compute attention weighted document embeddings - context matrix
         # Where c_i represents the document context vector for the i-th label in the label space
         # C ∈ R^nxd, where n: number of labels and d: latent dimension of CNN/LSTM model
-        C = A.matmul(V)
+        C = A.matmul(V.permute(0, 2, 1))
         return C, A
 
+
+class AlternateAttention(nn.Module):
+    """
+    Alternate attention mechanism as proposed by Bi et al. 2020 - Imbalanced Chinese Multi-label Text Classification
+    Based on Alternating Attention (https://aclanthology.org/2020.paclic-1.42/)
+
+    Parameters
+    ----------
+    input_dim : int
+        Number of features of input
+    output_dim : int
+        Number of features of output - number of attention hops (1 hop = 1 attention score vector)
+        could be number of labels in label space |L|
+    scale : bool; default=False
+        Flag indicating if energy scores (QxK.T) should be scaled
+
+    """
+
+    def __init__(self,
+                 input_dim,
+                 output_dim,
+                 scale):
+        super().__init__()
+        self._input_dim = input_dim
+        self._output_dim = output_dim
+        self._scale = scale
+
+        # Init target weights also considered to be the query embedding matrix Q ∈ R^nxd
+        # where n: n_labels and d: output feature of latent representation of documents using CNN or LSTM
+        # First alternating attention head - M
+        self.M = nn.Linear(in_features=self._input_dim,
+                           out_features=self._output_dim)
+        nn.init.xavier_uniform_(self.M.weight)
+
+        # Second alternating attention head - N
+        self.N = nn.Linear(in_features=self._input_dim,
+                           out_features=self._output_dim)
+        nn.init.xavier_uniform_(self.N.weight)
+
+    def forward(self, K: torch.Tensor, V: torch.Tensor) -> Tuple[torch.Tensor]:
+        """
+        Forward pass of alternating attention heads
+
+        Parameters
+        ----------
+        K : torch.Tensor
+            Latent representation of input sequence with shape: [batch_size: b, sequence_length: l, latent_dim: d]
+            Key embeddings K ∈ R^lxd
+        V : torch.Tensor
+            Latent representation of input sequence with shape: [batch_size: b, sequence_length: l, latent_dim: d]
+            Value embeddings V ∈ R^lxd
+
+        Returns
+        -------
+        C : torch.Tensor
+            Context matrix C - adjusted document embeddings
+            where c_i represents the context vector for the i-th label in the label space
+        A : torch.Tensor
+            Attention weight matrix A containing the attention scores
+            where a_i represents the attention weight for the i-th label in the label space
+
+        """
+        # Compute energy scores for both alternating attention heads
+        # Head 1: EM - dot product of query embedding matrix Q and key embedding matrix K: QxK.T
+        # where em_i represents the energy score for i-th attention vector; every second value is set to 0
+        # Head 2: EN - dot product of query embedding matrix Q and key embedding matrix K: QxK.T
+        # where en_i represents the energy score for i-th attention vector; every other second value is set to 0
+        # E ∈ R^nxl, where n: dimension of attention vector and l: sequence length
+        if self._scale:
+            EM = self.M.weight.matmul(K) / np.sqrt(self._embedding_dim)
+            EN = self.N.weight.matmul(K) / np.sqrt(self._embedding_dim)
+        else:
+            EM = self.M.weight.matmul(K)
+            EN = self.N.weight.matmul(K)
+
+        # Compute attention weights matrix A using a distribution function g (here softmax)
+        # where a_i represents the attention weights for the i-th label in the label space
+        # A ∈ R^nxl, where n: number of labels and l: sequence length
+        AM = F.softmax(input=EM, dim=2)
+        AN = F.softmax(input=EN, dim=2)
+
+        # Create mask to set alternating elements in AM and AN to 0
+        mask_m = torch.randn(AM.size()).bool()
+        for i in range(AM.size()[-1]):
+            if i % 2 != 0:
+                mask_m[:, :, i] = False
+        AM = AM.mul(mask_m)  # set every second element to 0 starting at index 1
+
+        mask_n = torch.randn(AN.size()).bool()
+        for i in range(AN.size()[-1]):
+            if i % 2 == 0:
+                mask_n[:, :, i] = False
+        AN = AN.mul(mask_n)  # set every second element to 0 starting at index 0
+
+        # Combine AM and AN
+        A = F.relu(AM + AN)
+
+        # Compute attention weighted document embeddings - context matrix
+        # Where c_i represents the document context vector for the i-th label in the label space
+        # C ∈ R^nxd, where n: number of labels and d: latent dimension of CNN/LSTM model
+        C = A.matmul(V.permute(0, 2, 1))
+        return C, A
