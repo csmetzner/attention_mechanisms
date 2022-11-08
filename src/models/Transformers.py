2b107ca945464a39b1b03e5074e3c2c26cec6c46
cmetzner93 <cmetzner@vols.utk.edu>
2022-11-08 11:47:10 -0500

    remove unneccessary attribute assignments

--- src/models/Transformers.py
+++ src/models/Transformers.py
@@ -107,16 +107,6 @@ class TransformerModel(nn.Module):
                                              cat_embedding_matrix=self._cat_embedding_matrix,
                                              code2cat_map=self._code2cat_map)
 
-            # After attention layer initialization we can retrieve the initial Q matrix
-            # .clone() is required to
-            if self._att_module.split('_')[0] == 'hierarchical':  # hierarchical_random / hierarchical_pretrained
-                # high-level query embedding matrix - categories
-                self._Q_init_cat = self.attention_layer.attention_layer.Q1.weight.detach().clone().cpu().numpy()
-                # low-level query embedding matrix - medical codes
-                self._Q_init = self.attention_layer.attention_layer.Q2.weight.detach().clone().cpu().numpy()
-            else:  # random / pretrained
-                self._Q_init = self.attention_layer.attention_layer.Q.weight.detach().clone().cpu().numpy()
-
         # Init output layer
         self.output_layer = nn.Linear(in_features=self._latent_doc_dim,
                                       out_features=self._n_labels)
